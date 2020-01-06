004f50753d6a28d0e4a8d23413d74fdd9f5bd81a
ivanalbizu <ivan.albizu@gmail.com>
2020-01-06 11:30:56 +0100

    Refactor

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const container = document.querySelector('.js-container');
-  
   const viewport = document.querySelector('.viewport');
+  
+  const container = document.querySelector('.js-container');
   const thumbs = document.querySelectorAll('.thumbs .thumb');
   const navs = document.querySelectorAll('.js-navigation');
   
@@ -13,11 +13,15 @@ document.addEventListener('DOMContentLoaded', () => {
   if (container && viewport && thumbs) {
     insertBefore(createFrameSlides(thumbs), container);
     container.appendChild(createFragment(row, col));
+
     setDelay(timeout);
+
     thumbs.forEach(anime => {
-      anime.addEventListener('click', handlerTransition.bind(this, timeout), false);
+      anime.addEventListener('click', handlerThumbs.bind(this, timeout), false);
     });
-    document.addEventListener('input', handlerBackground, false);
+
+    document.addEventListener('input', handlerCustomBackground, false);
+    
     navs.forEach(nav => {
       nav.addEventListener('click', handlerNavigation.bind(this, nav, timeout), false);
     });
@@ -25,83 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-const statusNavigation = el => {
-  if (!el.nextElementSibling) document.querySelector('.js-next').style.display = 'none';
-  else document.querySelector('.js-next').style.display = 'flex';
-  
-  if (!el.previousElementSibling) document.querySelector('.js-prev').style.display = 'none';
-  else document.querySelector('.js-prev').style.display = 'flex';
-}
 
-const handlerTransition = (timeout) => {
-  transitionTo(event.target, timeout);
-}
-
-const transitionTo = (to, timeout) => {
-  document.body.classList.add('js-animating');
-  document.querySelector('.thumb--active').classList.remove('thumb--active');
-
-  const particles = document.querySelectorAll('.js-container .item');
-  particles.forEach(item => item.classList.add('particles'))
-  
-  to.classList.add('thumb--active');
-  statusNavigation(to);
-  
-  const current = document.querySelector('.slide--active');
-  current.classList.add('fade-out');
-  
-  const slide = document.querySelector('#'+to.getAttribute('data-slide'));
-  slide.classList.add('fade-in');
-  
-  setTimeout(() => {
-    document.body.classList.remove('js-animating');
-    current.classList.remove(...['fade-out', 'slide--active']);
-    slide.classList.remove('fade-in');
-    slide.classList.add('slide--active');
-    particles.forEach(item => item.classList.remove('particles'))
-  }, timeout);
-}
-
-const handlerBackground = () => {
-  if (event.target.id === 'blend-mode') {
-    document.querySelectorAll('.viewport .slide').forEach(slide => {
-      slide.style.mixBlendMode = `${event.target.value}`;
-    });
-  } else if (event.target.id === 'bg-mode') {
-    document.querySelector('.viewport').style.backgroundColor = `${event.target.value}`;
-  } else {
-    return;
-  }
-}
-
-const handlerNavigation = (nav, timeout) => {
-  let active = null;
-  if (nav.classList.contains('js-next')) {        
-    active = document.querySelector('.thumb--active').nextElementSibling;
-  } else {
-    active = document.querySelector('.thumb--active').previousElementSibling;
-  }
-
-  transitionTo(active, timeout)
-}
-
-const setDelay = timeout => {
-  let style = document.createElement('style');
-  document.head.appendChild(style);
-  
-  const items = getComputedStyle(document.documentElement).getPropertyValue('--items');
-
-  for (let index = 0; index <= items; index++) {
-    const item = `.item--${index} {
-      animation-delay: ${index*100}ms;
-      animation-duration: ${timeout-index*100}ms;
-    }`;    
-    style.sheet.insertRule(item);
-  }
-}
-
-
-//Manupulaciones del DOM
 const createFrameSlides = thumbs => {
   const fragment = new DocumentFragment();
   thumbs.forEach((thumb, index) => {
@@ -119,6 +47,7 @@ const createFrameSlides = thumbs => {
   return fragment;
 }
 
+
 const elFactory = (type, attributes, ...children) => {
   const el = document.createElement(type)
 
@@ -133,6 +62,10 @@ const elFactory = (type, attributes, ...children) => {
 
   return el
 }
+
+
+const insertBefore = (el, referenceNode) => referenceNode.parentNode.insertBefore(el, referenceNode);
+
 
 const createFragment = (row, col) => {
   let fragment = new DocumentFragment();
@@ -158,4 +91,81 @@ const createFragment = (row, col) => {
   return fragment;
 }
 
-const insertBefore = (el, referenceNode) => referenceNode.parentNode.insertBefore(el, referenceNode);
+
+const setDelay = timeout => {
+  let style = document.createElement('style');
+  document.head.appendChild(style);
+  
+  const items = getComputedStyle(document.documentElement).getPropertyValue('--items');
+
+  for (let index = 0; index <= items; index++) {
+    const item = `.item--${index} {
+      animation-delay: ${index*100}ms;
+      animation-duration: ${timeout-index*100}ms;
+    }`;    
+    style.sheet.insertRule(item);
+  }
+}
+
+
+const statusNavigation = el => {
+  if (!el.nextElementSibling) document.querySelector('.js-next').style.display = 'none';
+  else document.querySelector('.js-next').style.display = 'flex';
+  
+  if (!el.previousElementSibling) document.querySelector('.js-prev').style.display = 'none';
+  else document.querySelector('.js-prev').style.display = 'flex';
+}
+
+
+const handlerThumbs = (timeout) => transitionTo(event.target, timeout);
+
+
+const handlerNavigation = (nav, timeout) => {
+  let active = null;
+  if (nav.classList.contains('js-next')) {        
+    active = document.querySelector('.thumb--active').nextElementSibling;
+  } else {
+    active = document.querySelector('.thumb--active').previousElementSibling;
+  }
+
+  transitionTo(active, timeout)
+}
+
+
+const transitionTo = (to, timeout) => {
+  document.body.classList.add('js-animating');
+  document.querySelector('.thumb--active').classList.remove('thumb--active');
+
+  const particles = document.querySelectorAll('.js-container .item');
+  particles.forEach(item => item.classList.add('particles'))
+  
+  to.classList.add('thumb--active');
+  statusNavigation(to);
+  
+  const current = document.querySelector('.slide--active');
+  current.classList.add('fade-out');
+  
+  const slide = document.querySelector('#'+to.getAttribute('data-slide'));
+  slide.classList.add('fade-in');
+  
+  setTimeout(() => {
+    document.body.classList.remove('js-animating');
+    current.classList.remove(...['fade-out', 'slide--active']);
+    slide.classList.remove('fade-in');
+    slide.classList.add('slide--active');
+    particles.forEach(item => item.classList.remove('particles'));
+  }, timeout);
+}
+
+
+const handlerCustomBackground = () => {
+  if (event.target.id === 'blend-mode') {
+    document.querySelectorAll('.viewport .slide').forEach(slide => {
+      slide.style.mixBlendMode = `${event.target.value}`;
+    });
+  } else if (event.target.id === 'bg-mode') {
+    document.querySelector('.viewport').style.backgroundColor = `${event.target.value}`;
+  } else {
+    return;
+  }
+}
